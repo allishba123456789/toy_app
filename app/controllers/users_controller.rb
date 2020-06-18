1e63a7dd5d71ea824cd7ea987e62dc15642f9008
allishba123456789 <alishbaather321@gmail.com>
2020-06-18 11:53:07 +0500

    final toy_app with sqlite and postgresql

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      puts params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email)
     end
 end
